130f7bc1af4322ea4459818eb357129833b60426
Dharunkumar V <dharunkumarv@Dharuns-MacBook-Air.local>
2026-01-21 14:45:34 +0530

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import { db } from "@dynamic-qr/db";
+import * as schema from "@dynamic-qr/db/schema/auth";
+import { env } from "@dynamic-qr/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
